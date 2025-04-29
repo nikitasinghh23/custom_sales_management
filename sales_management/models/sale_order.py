ce5e0912ee00ea48526eaf5ac7bf35f51de4f83b
niktasinghh23 <nikita.singh@mail.codetrade.io>
2025-04-29 14:40:22 +0530

    Initial commit

--- sales_management/models/sale_order.py
+++ sales_management/models/sale_order.py
@@ -14,22 +14,15 @@ class SaleOrder(models.Model):
 
     def _prepare_invoice(self):
         invoice_vals = super()._prepare_invoice()
-        print("----invoice_vals---",invoice_vals)
         invoice_vals.update({
             'reference_id': self.reference_id,
         })
-        print("----invoice_vals--invoice_vals--",invoice_vals)
         return invoice_vals
 
     def write(self, vals):
         result = super().write(vals)
-        print("---result",result)
         if 'reference_id' in vals:
-            print("vals",vals)
             for order in self:
-                print("----self--",self)
-                print("----order--", order)
                 for invoice in order.invoice_ids:
-                    print("----invoice---",invoice)
                     invoice.reference_id = vals['reference_id']
         return result
